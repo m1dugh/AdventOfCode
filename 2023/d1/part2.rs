147b7e5170a4704037e2fe358c36ab838a1d0664
m1dugh <romain103paris@gmail.com>
2023-12-01 23:29:14 +0100

    fix(d1): fixes broken part2

--- 2023/d1/part2.rs
+++ 2023/d1/part2.rs
@@ -3,95 +3,24 @@ use std::env;
 use std::fs;
 use std::vec::Vec;
 
-fn find_substring(main: &str, sub: &str, start: u32) -> i64
-{
-    let mut i: usize = start as usize;
-    let mut subi: usize = 0;
-    let mut res = -1;
-    while i < main.len()
-    {
-        if subi == sub.len()
-        {
-            return res;
-        }
-
-        if sub.chars().nth(subi) == main.chars().nth(i)
-        {
-            if subi == 0
-            {
-                res = i as i64;
-            }
-            subi += 1;
-        }
-        else
-        {
-            subi = 0;
-        }
-
-        i += 1;
-    }
-
-    if subi == sub.len()
-    {
-        res
-    }
-    else
-    {
-        -1
-    }
-
-}
-
-static NUMBERS: &'static [&'static str] = &["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-
 fn get_characters(line: &str) -> Vec<u32>
 {
-    let str = line.to_lowercase();
-    let mut numbers: Vec<u32> = Vec::<u32>::new();
+    let str = line.to_lowercase().to_string();
+    let str = str.replace("one", "o1e");
+    let str = str.replace("two", "t2e");
+    let str = str.replace("three", "t3e");
+    let str = str.replace("four", "f4r");
+    let str = str.replace("five", "f5e");
+    let str = str.replace("six", "s6x");
+    let str = str.replace("seven", "s7n");
+    let str = str.replace("eight", "e8t");
+    let str = str.replace("nine", "n9e");
 
-    let mut i = 0;
+    let res = str.chars()
+        .filter_map(|x| x.to_digit(10))
+        .collect();
 
-    while i < str.len()
-    {
-        let mut new_start_index = str.len();
-        let mut value: usize = 0;
-        let mut sublen = 0;
-        for (pos, e) in NUMBERS.iter().enumerate()
-        {
-            let tmp = find_substring(str.as_str(), e, i as u32);
-            if tmp != -1 && (tmp as usize) < new_start_index
-            {
-                new_start_index = tmp as usize;
-                value = pos + 1;
-                sublen = e.len();
-            }
-        }
-
-        if value > 0
-        {
-            let mut sub: Vec<u32> = str.chars()
-                .skip(i)
-                .take(new_start_index - i)
-                .filter_map(|x| x.to_digit(10))
-                .collect();
-
-            numbers.append(&mut sub);
-            numbers.push(value as u32);
-            i = new_start_index + sublen;
-        }
-        else
-        {
-            let mut sub: Vec<u32> = str.chars()
-                .skip(i)
-                .filter_map(|x| x.to_digit(10))
-                .collect();
-
-            numbers.append(&mut sub);
-            break;
-        }
-    }
-
-    numbers
+    res
 }
 
 fn main() -> io::Result<()> 
