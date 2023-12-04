afbddabacf8a2fc5b440ba2188be4d78bd6290e3
Romain Le Miere <romain.le-miere@r06p05.cisco.kb.sm.cri.epita.fr>
2023-12-05 00:08:30 +0100

    feat(d4): implements part2

---
+++ 2023/d4/part2.rs
@@ -0,0 +1,148 @@
+use std::cmp::PartialEq;
+use std::io::{self, BufRead, BufReader, Read};
+use std::fs;
+use std::env;
+use std::fmt;
+use std::collections::VecDeque;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Card
+{
+    id: u32,
+    winings: Vec<u32>,
+    values: Vec<u32>,
+}
+
+impl Card
+{
+    fn get_results(&self) -> u32
+    {
+        self.values
+            .iter()
+            .filter(|v| self.winings.contains(&v))
+            .count() as u32
+    }
+
+    fn extract_numbers(line: &str) -> Vec<u32>
+    {
+        line
+            .split(" ")
+            .filter_map(|v| match v.parse() {
+                Ok(v) => Some(v),
+                Err(_) => None,
+            })
+            .collect::<Vec<u32>>()
+    }
+
+    fn parse(line: &str) -> Result<Card, &str>
+    {
+        if !line.starts_with("Card ")
+        {
+            return Err("Invalid line");
+        }
+
+        let line = line.strip_prefix("Card ").unwrap();
+        let to_strip: usize;
+        let id: u32;
+        if let Some(index) = line.chars().position(|c| c == ':') {
+            id = match line
+                .chars()
+                .take(index)
+                .collect::<String>()
+                .trim()
+                .parse() {
+                Ok(val) => val,
+                Err(_) => return Err("Invalid card number"),
+            };
+            to_strip = index + 1;
+        }
+        else
+        {
+            return Err("Missing colon");
+        }
+
+        let line_string = line.chars().skip(to_strip).collect::<String>();
+
+        let parts = line_string
+            .split("|")
+            .collect::<Vec<&str>>();
+        if parts.len() != 2
+        {
+            return Err("Invalid line");
+        }
+
+        Ok(Card {
+            id: id,
+            winings: Card::extract_numbers(parts[0]),
+            values: Card::extract_numbers(parts[1]),
+        })
+    }
+}
+
+fn main() -> io::Result<()>
+{
+    if env::args().len() != 2
+    {
+        panic!("Usage: ./main <filename>");
+    }
+
+    let filename = env::args().nth_back(0).unwrap();
+
+    let file = fs::File::open(filename)?;
+
+    let mut reader = BufReader::new(file);
+
+    let lines = reader.lines()
+        .filter_map(|l| match l {
+            Err(_) => None,
+            Ok(v) => Some(v),
+        })
+        .collect::<Vec<String>>();
+
+    let cards = lines
+        .iter()
+        .filter_map(|l| match Card::parse(l.as_str()) {
+            Ok(v) => Some((v.id, v)),
+            Err(e) => {
+                println!("error on {l} => {e}");
+                None
+            },
+        })
+        .collect::<HashMap<u32, Card>>();
+
+    let mut values = HashMap::<u32, u32>::new();
+
+    let mut queue = VecDeque::<u32>::new();
+
+    let mut indices = cards
+        .keys()
+        .map(|&v| v)
+        .collect::<Vec<u32>>();
+
+    indices.sort_by(|a, b| b.cmp(a));
+    indices
+        .iter()
+        .map(|v| (v, &cards[v]))
+        .for_each(|(&i, card)| {
+            
+            let results = card.get_results();
+            let mut final_res: u32 = 1;
+            for i in 1..results + 1
+            {
+                final_res += values[&(card.id + i)];
+            }
+
+            values.insert(i, final_res);
+        });
+    println!("{values:?}");
+
+    let res = values.values()
+        .map(|&v| v)
+        .reduce(|a, b| a + b)
+        .unwrap_or(0);
+
+    println!("{res}");
+
+    Ok(())
+}
